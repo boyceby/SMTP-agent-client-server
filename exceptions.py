ebd517e047cdd08c0368aa641c8b77be831afd9e
Boyce Young <54334676+boyceby@users.noreply.github.com>
2021-10-21 15:22:35 -0400

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,7 +1,7 @@
 """
 Exceptions
 """
- 
+
 class ConsumptionError(Exception):
     """Exception raised when 'consume' fails to match a given regexp pattern with the beginning of
         the remaining string, (and therefore also fails to consume it)."""
@@ -29,12 +29,12 @@ class EarlyTerminationError(Exception):
     """Exception raised when EOF is encountered before all required email data has been provided
         by the command-line user."""
     pass
-  
+
 class SocketError(Exception):
     """Exception raised when an any exception is encountered while creating, initiating a connection with,
     reading from, or writing to a socket."""
     pass
-  
+
 class SMTPBreakOfProtocolError(Exception):
     """Exception raised when a message breaking SMTP protocol has been sent from the client or received
     from the server."""
